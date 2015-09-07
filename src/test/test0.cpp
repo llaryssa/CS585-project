229c9a7653de2a4437d5f6593d9392fdd5b0b696
Laryssa Seabra <llaryssa@me.com>
2015-09-07 19:42:36 -0400

    Adding more trivial tests and trying to make travis work

--- src/test/test0.cpp
+++ src/test/test0.cpp
@@ -1,11 +1,31 @@
 // test0.cpp
 #include <gtest/gtest.h>
  
-TEST( SampleTest, AssertionTrue ) {
+inline
+bool isEven( int n )
+{
+	return ( n % 2 == 0 );
+}
+
+inline
+int squared ( int n )
+{
+	return n * n;
+}
+
+TEST( EqualTest, Trivial ) {
     ASSERT_EQ( 1, 1 );
+    ASSERT_EQ( false, false );
 }
 
-TEST( SampleTest2, AssertionTrue ) {
-	EXPECT_GT( 7, 6 );
+TEST( EvenTest, PositiveInputs ) {
+	EXPECT_TRUE( isEven( 6 ) );
+	EXPECT_FALSE( isEven( 33 ) );
+	EXPECT_FALSE( isEven( 9 ) );
+	EXPECT_TRUE( isEven( 2 ) );
 }
 
+TEST( SquareTest, Eq ) {
+	ASSERT_EQ( squared( 1 ), squared ( 1 ) );
+	EXPECT_LT( 5, squared( 5 ) );
+}
