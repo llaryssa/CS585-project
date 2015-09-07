d43247dd640c6fe6f66e1c3db48e68f3ed6ab8eb
llaryssa <alsas@cin.ufpe.br>
2015-09-07 02:12:07 -0400

    Adding travis, updating the CMakeLists and adding test

--- game.m.cpp
+++ game.m.cpp
@@ -1,6 +1,8 @@
 // game.m.cpp
 #include <iostream>
 
+#include "gtest/gtest.h"
+
 int main()
 {
     std::cout << "Hello world!" << std::endl;
---
+++ src/test/test0.cpp
@@ -0,0 +1,11 @@
+// test0.cpp
+#include "gtest/gtest.h"
+ 
+TEST( SampleTest, AssertionTrue ) {
+    ASSERT_EQ( 1, 1 );
+}
+
+TEST( SampleTest2, AssertionTrue ) {
+	EXPECT_GT( 7, 6 );
+}
+
