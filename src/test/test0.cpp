daab18a0e1410981b0793d907e72287217fe3f7e
llaryssa <alsas@cin.ufpe.br>
2015-09-10 14:30:55 -0400

    Replacing tabs with spaces

--- src/test/test0.cpp
+++ src/test/test0.cpp
@@ -4,28 +4,31 @@
 inline
 bool isEven( int n )
 {
-	return ( n % 2 == 0 );
+    return ( n % 2 == 0 );
 }
 
 inline
 int squared ( int n )
 {
-	return n * n;
+    return n * n;
 }
 
-TEST( EqualTest, Trivial ) {
+TEST( EqualTest, Trivial ) 
+{
     ASSERT_EQ( 1, 1 );
     ASSERT_EQ( false, false );
 }
 
-TEST( EvenTest, PositiveInputs ) {
-	EXPECT_TRUE( isEven( 6 ) );
-	EXPECT_FALSE( isEven( 33 ) );
-	EXPECT_FALSE( isEven( 9 ) );
-	EXPECT_TRUE( isEven( 2 ) );
+TEST( EvenTest, PositiveInputs ) 
+{
+    EXPECT_TRUE( isEven( 6 ) );
+    EXPECT_FALSE( isEven( 33 ) );
+    EXPECT_FALSE( isEven( 9 ) );
+    EXPECT_TRUE( isEven( 2 ) );
 }
 
-TEST( SquareTest, Eq ) {
-	ASSERT_EQ( squared( 1 ), squared ( 1 ) );
-	EXPECT_LT( 5, squared( 5 ) );
+TEST( SquareTest, Eq ) 
+{
+    ASSERT_EQ( squared( 1 ), squared ( 1 ) );
+    EXPECT_LT( 5, squared( 5 ) );
 }
